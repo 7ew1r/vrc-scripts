775000d0c71eecce0f446434aebeb068d5920928
7ew1r <art.sht.0317@gmail.com>
2025-04-27 22:11:38 +0900

    update version

--- const.example.ps1
+++ const.example.ps1
@@ -1,6 +1,5 @@
-# Unity のバージョンとインストールパスを追記し「const.ps1」にリネーム
+# Rename this file to `const.ps1`
 
 $UNITY_VERSION_EXE = @{
-    "2018.4.20f1" = "C:\Program Files\Unity\Hub\Editor\2018.4.20f1\Editor\Unity.exe";
-    "2019.4.31f1" = "C:\Program Files\Unity\Hub\Editor\2019.4.31f1\Editor\Unity.exe";
+    "2022.3.22f1" = "F:\Unity\2022.3.22f1\Editor\Unity.exe"
 }
